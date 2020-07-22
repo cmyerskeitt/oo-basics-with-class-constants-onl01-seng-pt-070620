3bf1d920132d62dde00f1bcf521546155b6fe53a
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-22 22:04:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count,
   attr_reader :title
   
   GENRES = []
@@ -7,6 +7,11 @@ class Book
   def initialize(title)
     @title = title
   end
+
+
+
+
+
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
