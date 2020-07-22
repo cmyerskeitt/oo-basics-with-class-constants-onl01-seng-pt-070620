bee498b52f5165c97ea26cd720e540fc2cec5907
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-22 22:09:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,6 +1,8 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  
+  BRAND = []
 
   def initialize(brand)
     @brand = brand
@@ -11,4 +13,5 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  
 end
